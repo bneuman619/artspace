a76a5aff5f706ab611fc735dae3f2c52a688bfa5
Laura <lleecrowe@gmail.com>
2014-02-27 20:19:09 -0600

    created users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  private
+
   def current_user
     @_current_user ||= session[:current_user_id] && User.find(session[:current_user_id])
   end
