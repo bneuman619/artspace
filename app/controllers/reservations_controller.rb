7576941c28a352bb1e18590174b6394b2790addb
stevenspiel <stevenspiel@gmail.com>
2014-02-28 16:22:04 -0600

    Wrote Reservations#create logic

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,17 @@
 class ReservationsController < ApplicationController
 
   def create
+    make_reservations(params["data"])
+    @space = Space.find(params[:spaceId])
+    reservation_data = params["data"]
+    reservations = reservation_data.values.collect do |reservation_datum|
+      @space.reservations.create(
+        renter: current_user,
+        start_time: reservation_datum[:start],
+        end_time: reservation_datum[:end])
+    end
+
+    render json: "SUCCESS!"
   end
 
   def show
