80ea5650442c062db32f7a8cd5823d86f2033676
Laura <lleecrowe@gmail.com>
2014-02-28 10:46:21 -0600

    seeds for uses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Use.create(category: "dance")
+Use.create(category: "audio")
+Use.create(category: "studio art")
+Use.create(category: "theater")
+Use.create(category: "rehearsal")
+Use.create(category: "performance")
+Use.create(category: "class")
+Use.create(category: "audition")
+Use.create(category: "photoshoot")
+Use.create(category: "video")
+Use.create(category: "exhibition")
+Use.create(category: "reading")
+Use.create(category: "screening")
+Use.create(category: "any")
