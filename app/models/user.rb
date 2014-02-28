f4e5f89023d06e55d291912920095edd3f2da8d9
Laura <lleecrowe@gmail.com>
2014-02-27 18:32:45 -0600

    corrected renter association

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   has_many :spaces_created, class_name: "Space", foreign_key: "creator_id"
 
   has_many :reservations, foreign_key: :renter_id
-  has_many :spaces_rented, through: :reservations
+  has_many :rentals, through: :reservations
 end
