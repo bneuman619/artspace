f12893979f2f753159f0088401905fa1d0de6247
Laura <lleecrowe@gmail.com>
2014-02-28 11:58:44 -0600

    added availability seeds and changed time fields in migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -87,6 +87,16 @@ Admin.create(administrator_id: 4,
   				)
 end
 
+#------------------------Seed Availabilities---------------------------------------
+
+Space.all.each do |space|
+  space.availabilities.create(space_id: space.id,
+  							  start_time: '09:00',
+  							  end_time: '17:00',
+  							  day: 1
+  							)
+end
+
 
 
 
