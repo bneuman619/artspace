44bdf3057abb1c8243ddee402da821a1d66f42f0
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-01 12:13:24 -0600

    Added #length_in_hours method to Reservation model for calculating fees

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ActiveRecord::Base
   belongs_to :renter, class_name: "User"
   belongs_to :space
+
+  def length_in_hours
+    (self.end_time - self.start_time) / 1.hour
+  end
 end
