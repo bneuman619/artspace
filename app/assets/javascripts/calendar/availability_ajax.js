3d4797712041f91dfbff04292852ff50a4e68a2b
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-02 16:25:42 -0600

    Readding error check to edit_availability ajax

--- app/assets/javascripts/calendar/availability_ajax.js
+++ app/assets/javascripts/calendar/availability_ajax.js
@@ -26,9 +26,14 @@ $(document).ready(function() {
         url: '/spaces/availabilities/update',
         dataType: "json",
         success: function(response) {
+          if(response.status == 'error') {
+            alert("There was a problem with your availabilities");
+          }
+          else {
           window.location = "/users/" + $("#edit_availabilities").data().userId + "/manage";
-          console.log(response);
+          }
         },
+
         error: function(xhr, error) {
           alert("there was an error");
         }
