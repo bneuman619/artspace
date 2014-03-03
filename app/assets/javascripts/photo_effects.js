1fd20b6bace9daa488ad6e78db9822dfb48dec11
Laura <lleecrowe@gmail.com>
2014-03-03 00:20:23 -0600

    added slow animation and partial opacity

--- app/assets/javascripts/photo_effects.js
+++ app/assets/javascripts/photo_effects.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-  // $('img').hover(function(){
 	$("div.space_holder").hover(function(){
-	  $(this).closest('div').find('.hidden_text').show();
+	  $(this).closest('div').find('.hidden_text').show("slow");
 	},function(){
-	  $(".hidden_text").hide();
+	  $(".hidden_text").hide("slow");
 	});
 });
