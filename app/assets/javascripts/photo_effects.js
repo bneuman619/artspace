50551874093c90ccc55bb16fcd5fa24af49eddab
Laura <lleecrowe@gmail.com>
2014-03-03 17:23:18 -0600

    more accurate scroll

--- app/assets/javascripts/photo_effects.js
+++ app/assets/javascripts/photo_effects.js
@@ -1,48 +1,33 @@
 $(document).ready(function(){
+
+//reveal text on hover-------------------------------------------
 	$("div.space_holder").hover(function(){
 	  $(this).closest('div').find('.hidden_text').show("slow");
 	},function(){
 	  $(".hidden_text").hide("slow");
 	});
 
-
-	// $("#forward").click(function(event){
-	//   console.log("hello");
-	//   var first = $('.space_holder:first')
-
-
-
-
-    var $item = $('div.space_holder'), //Cache your DOM selector
-        visible = 2, //Set the number of items that will be visible
-        index = 0, //Starting index
-        endIndex = ( $item.length / visible ) - 1; //End index
+//scroll when you click arrows-----------------------------------
+    var $item = $('div.space_holder'), 
+        visible = 2, 
+        index = 0, 
+        endIndex = ( $item.length / visible ) - 1; 
     
     $('div#forward').click(function(){
         if(index < endIndex ){
           index++;
-          $item.animate({'left':'-=1040px'});
+          $item.animate({'left':'-=1030px'});
         }
     });
     
     $('div#back').click(function(){
         if(index > 0){
           index--;            
-          $item.animate({'left':'+=1040px'});
+          $item.animate({'left':'+=1030px'});
         }
     });
 
 
 
 
-
-
-
-
-
-
-
-
-
-	// });
 });
