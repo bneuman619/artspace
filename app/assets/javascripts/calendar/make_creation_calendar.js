cb000bd9a5e4ff955f90863055e31783f564c072
Sam Blackman <sblackman@gmail.com>
2014-03-03 10:39:23 -0600

    Removed business hour bold on availabilities#new calendar; commented out unneeed code

--- app/assets/javascripts/calendar/make_creation_calendar.js
+++ app/assets/javascripts/calendar/make_creation_calendar.js
@@ -2,9 +2,21 @@ modifiedEvents = []
 
 function make_creation_calendar(events_input) {
   var calendar = {
-    timeslotsPerHour: 4,
     scrollToHourMillis : 0,
-    defaultFreeBusy : {free: true},
+    timeslotsPerHour: 2,
+    timeslotHeight: 20,
+    defaultFreeBusy: {free: true},
+    displayFreeBusys: false,
+    buttons: false,
+    showHeader: false,
+    displayOddEven: true,
+    allowEventDelete: true,
+    daysToShow: 7,
+    headerSeparator: ' ',
+    useShortDayNames: false,
+    allowCalEventOverlap: false,
+    dateFormat: 'd F y',
+    businessHours: false,
 
     height: function($calendar){
       return $(window).height() - $('h1').outerHeight(true);
@@ -20,30 +32,29 @@ function make_creation_calendar(events_input) {
     },
 
     eventDrop : function(newCalEvent, oldCalEvent, element) {
-      for(i = 0; i < modifiedEvents.length; i++) {
-        if (modifiedEvents[i].id == newCalEvent.id) {
-          modifiedEvents[i] = newCalEvent;
-          return true;
-        }
-      }
-      modifiedEvents.push(newCalEvent);
+      // for(i = 0; i < modifiedEvents.length; i++) {
+      //   if (modifiedEvents[i].id == newCalEvent.id) {
+      //     modifiedEvents[i] = newCalEvent;
+      //     return true;
+      //   }
+      // }
+      // modifiedEvents.push(newCalEvent);
     },
 
     eventNew : function(calEvent, $event, FreeBusyManager, calendar) {
-      alert('You\'ve added a new event. You would capture this event, add the logic for creating a new event with your own fields, data and whatever backend persistence you require.');
+      // calEvent.id = "new_event" +'_'+ calEvent.start.getTime();
 
-      calEvent.id = "new_event" +'_'+ calEvent.start.getTime();
-
-      modifiedEvents.push(calEvent);
+      // modifiedEvents.push(calEvent);
     },
 
     eventDelete: function(calEvent, element, dayFreeBusyManager, calendar, clickEvent) {
       if (confirm('You want to delete this reservation?')) {
-        for(i = 0; i < modifiedEvents.length; i++) {
-          if (modifiedEvents[i].id == calEvent.id) {
-            modifiedEvents.splice(i, 1);
-          }
-        }
+        // for(i = 0; i < modifiedEvents.length; i++) {
+        //   if (modifiedEvents[i].id == calEvent.id) {
+        //     modifiedEvents.splice(i, 1);
+        //   }
+        // }
+        removeEvent(calEvent, calendar);
       }
     },
 
@@ -54,19 +65,6 @@ function make_creation_calendar(events_input) {
      getHeaderDate: function(date, calendar) {
       return get_day(date);
     },
-
-    buttons: false,
-    showHeader: false,
-
-    displayOddEven: true,
-    displayFreeBusys: false,
-    allowEventDelete: true,
-    daysToShow: 7,
-    switchDisplay: {'1 day': 1, 'full week': 7},
-    headerSeparator: ' ',
-    useShortDayNames: true,
-    allowCalEventOverlap: false,
-    dateFormat: 'd F y',
   }
 
   return calendar;
