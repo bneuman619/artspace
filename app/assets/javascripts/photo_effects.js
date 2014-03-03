2ac230a81c90b45c9625c1cdd40f245272298358
Laura <lleecrowe@gmail.com>
2014-03-02 23:41:10 -0600

    jquery reveals text

--- app/assets/javascripts/photo_effects.js
+++ app/assets/javascripts/photo_effects.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+  // $('img').hover(function(){
+	$("div.space_holder").hover(function(){
+	  $(this).closest('div').find('.hidden_text').show();
+	},function(){
+	  $(".hidden_text").hide();
+	});
 });
