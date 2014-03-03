e20f2ef35c8ce828d3b0907fa5b2314664a6fecc
Sam and Lydia <lydiakoller1@gmail.com>
2014-03-03 13:51:03 -0600

    removes pic on click

--- app/assets/javascripts/upload_image.js
+++ app/assets/javascripts/upload_image.js
@@ -7,8 +7,17 @@ $(function() {
     }
   );
   $('#s3-uploader').bind('s3_upload_complete', function(e, content) {
-    $("#uploads_container").append('<img src=' + content['url'] + ' height="125" width="125">')
+    $("#uploads_container").append('<img class="remove_img" src=' + content['url'] + ' height="125" width="125">')
     imgArray.push(content['url'] )
     $("#form_container").append('<input type="hidden" name="pic_url" value=' + imgArray + '>')
+
   });
+
+  $('#uploads_container').on('click', function() {
+    if ($(event.target).hasClass('remove_img')) {
+      var index = imgArray.indexOf($(event.target).attr('src'));
+        imgArray.splice(index, 1)
+      $(event.target).hide();
+    }
+  })
 });
