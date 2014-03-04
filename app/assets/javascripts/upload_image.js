058ec038f25e2e4adadbc22bf222f3661afd8e6d
Sam and Lydia <lydiakoller1@gmail.com>
2014-03-04 15:03:29 -0600

    need pictures to create acct

--- app/assets/javascripts/upload_image.js
+++ app/assets/javascripts/upload_image.js
@@ -1,16 +1,18 @@
 $(function() {
   imgArray = []
+  var delete_max = $(".old_pic").length;
+
   $('#s3-uploader').S3Uploader(
     {
       remove_completed_progress_bar: false,
       progress_bar_target: $('#uploads_container')
     }
   );
+
   $('#s3-uploader').bind('s3_upload_complete', function(e, content) {
     $("#uploads_container").append('<img class="remove_img" src=' + content['url'] + ' height="125" width="125">')
     imgArray.push(content['url'] )
     $("#form_container").append('<input type="hidden" name="pic_url" value=' + imgArray + '>')
-
   });
 
   $('#uploads_container').on('click', function() {
@@ -21,10 +23,23 @@ $(function() {
     }
   })
 
+  $(".pic_validate").on("click", function(e) {
+    if (imgArray.length === 0) {
+      e.preventDefault();
+      alert("You must upload at least 1 picture.");
+    }
+  });
+
   $(".old_pic").click(function() {
     var img_id = $(this).attr('name')
-    $('#' + img_id).fadeOut();
-    $.post( "/delete_pic", {id: img_id} )
+    if ( delete_max > 1 ) {
+      delete_max --
+      $('#' + img_id).fadeOut();
+      $.post( "/delete_pic", {id: img_id} )
+    }
+    else {
+      alert("You must have at least 1 picture.")
+    }
   })
 
 });
