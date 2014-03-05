1d63c5ccffab1f3f42a948781e0458ce42d9dde7
Sam Blackman <sblackman@gmail.com>
2014-03-05 11:16:39 -0600

    Completely refactored JS reservation work

--- app/assets/javascripts/calendar/reservation_ajax.js
+++ app/assets/javascripts/calendar/reservation_ajax.js
@@ -9,24 +9,55 @@ $(document).ready(function() {
     }
 
     else {
-      send_reservation_info();
+      make_reservation();
     }
   });
 });
 
-function send_reservation_info() {
-  var data = {space_id: $("#make_reservation").data('space-id'),
-             reservations: get_events()};
+function make_reservation(token_key) {
+  var data = {
+    reservation_data: prepare_reservation_data(),
+    payment_info: prepare_payment_data(token_key)
+  };
 
-  $.post('/reservations', data, function(response) {
-    if(response.status == 'error') {
-      alert(response.error)
-    }
-
-    else {
-      var res_ids = response.reservation_ids.join(',');
-      window.location = "/reservations/confirmation?ids=" + res_ids + "&amount=0";
-    }
-  });
+  send_reservation(data);
 }
+
+function send_reservation(data) {
+  $.post('/reservations', data, reservation_response);
+}
+
+function reservation_response(response) {
+  if(response.status == 'error') {
+    alert(response.error)
+  }
+
+  else {
+    var res_ids = response.reservation_ids.join(',');
+    window.location = "/reservations/confirmation?ids=" + res_ids +"&amount=" + response.totalCharge;
+  }
+}
+
+function prepare_reservation_data() {
+  return {
+    space_id: $("#make_reservation").data('space-id'),
+    reservations: get_events()
+  };
+}
+
+function prepare_payment_data(token_key) {
+  var payment_data = {};
+
+  if (token_key) {
+    payment_data.token_key = token_key;
+    payment_data.amount = get_total_charge();
+    payment_data.description = $("#make_reservation").data().spaceTitle;
+  }
+  else {
+    payment_data.amount = 0;
+  }
+
+  return payment_data;
+}
+
 
