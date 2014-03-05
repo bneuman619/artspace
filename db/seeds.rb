18296b956f07981518c38e95dd84ff14e44c1653
Sam and Lydia <zappeln@gmail.com>
2014-03-05 16:26:25 -0600

    more pics for seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -138,7 +138,26 @@ Payment.create(creator_id: 1, secret_api_key: "sk_test_1bDMZ95bU3Xa3EdqVVkv5o1m"
 
 #----------------------Seed Images___________________________________________________
 
-IMG_ARRAY = [ "https://s3.amazonaws.com/rentspaces/uploads%2F1393793550915-avlx57x6sguy2e29-e4a45dc51eacfeb964193f208e908e2b%2Fimages.jpeg",
+IMG_ARRAY = [ "https://s3.amazonaws.com/rentspaces/uploads/1393785811893-n0yau8vrclo0qkt9-4c20bbe51412da2e07a6b3b216b998f7/download.jpeg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393786143147-0esjp61262zqia4i-54f4514197f477b174bab6c11a466241/dl2.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393786148919-dxo28k26efef80k9-54f4514197f477b174bab6c11a466241/tumblr_l00r7wG6xt1qzoh1b.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393786238790-gwip961oje3a0pb9-4ea79c2f649e7b192497a0571ab54669/south_space_large.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393786528482-pj9aezsu8qfflxr-dc20b9d73fdc6e87bab2b3935f8d2e2c/image_view.jpeg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393787711815-9ul6lz3r6gsnhfr-7f0797ab436b8098227f8d3abcf09c82/Original-Empty-Room-Resized.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393787719281-qbjt34jv4bpm0a4i-7f0797ab436b8098227f8d3abcf09c82/I%2Bbelieve%2Bit%2Bwould%2Bbe%2Bsomething%2Blike%2Bthis%2B_93d4703edf64676729594cc837f99582.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393790193377-49930nsk2urtfbt9-c423f8f12cc74fb17dc49585590f0ca5/177904033_80ebca4fd3_o.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393791233711-tqnasgt4sjg7gb9-1e54f0261d5ad5fbd9d89db420efef14/dance_space.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393793550915-avlx57x6sguy2e29-e4a45dc51eacfeb964193f208e908e2b/images.jpeg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393794127242-ugv41c3owv2yy14i-051bd9a4f9b4afe9b19fa26c4c4d15e8/1251910935-yd-a-528x343.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393793556754-hfngcnc56gmygb9-e4a45dc51eacfeb964193f208e908e2b/Rosana-empty-studio.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393794127242-ugv41c3owv2yy14i-051bd9a4f9b4afe9b19fa26c4c4d15e8/1251910935-yd-a-528x343.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393857056084-zvalncahh6ywrk9-a57e6ea588502e0309282eaf9698e8a5/mms_picture+copy.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393857071031-13nwxwzd1ln89f6r-a57e6ea588502e0309282eaf9698e8a5/mms_picture.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393861299877-44fahvii1j68ncdi-73c797abf11bbd6d22cfba3f2e4a4468/mms_picture+(1)+copy.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393861381975-bddxfzkxfyxh6w29-e876072773b4cce0fc818245fad93b75/mms_picture+(1).jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393861452673-r7pjok8clult0529-c4698ce628f9862e1d1e695d92055ea7/mms_picture+(2).jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393874804694-b8rgrf4rf3d9ggb9-62a79ae1c92acddfd4450b3883a66465/sanfran.jpg",
+              "https://s3.amazonaws.com/rentspaces/uploads/1393793550915-avlx57x6sguy2e29-e4a45dc51eacfeb964193f208e908e2b/images.jpeg",
               "https://s3.amazonaws.com/rentspaces/uploads%2F1393794116931-bkpqo7pvez2rzfr-051bd9a4f9b4afe9b19fa26c4c4d15e8%2FRosana-empty-studio.jpg",
               "https://s3.amazonaws.com/rentspaces/uploads%2F1393794127242-ugv41c3owv2yy14i-051bd9a4f9b4afe9b19fa26c4c4d15e8%2F1251910935-yd-a-528x343.jpg",
               "https://s3.amazonaws.com/rentspaces/uploads%2F1393793550915-avlx57x6sguy2e29-e4a45dc51eacfeb964193f208e908e2b%2Fimages.jpeg",
@@ -153,7 +172,7 @@ IMG_ARRAY = [ "https://s3.amazonaws.com/rentspaces/uploads%2F1393793550915-avlx5
             ]
 
 Space.all.each do |space|
-  2.times do
+  rand(2..4).times do
     Photo.create(space: space, url: IMG_ARRAY.sample)
   end
 end
@@ -161,25 +180,25 @@ end
 
 
 
-# https://s3.amazonaws.com/rentspaces/uploads/1393785811893-n0yau8vrclo0qkt9-4c20bbe51412da2e07a6b3b216b998f7/download.jpeg
-# https://s3.amazonaws.com/rentspaces/uploads/1393786143147-0esjp61262zqia4i-54f4514197f477b174bab6c11a466241/dl2.jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393786148919-dxo28k26efef80k9-54f4514197f477b174bab6c11a466241/tumblr_l00r7wG6xt1qzoh1b.jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393786238790-gwip961oje3a0pb9-4ea79c2f649e7b192497a0571ab54669/south_space_large.jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393786528482-pj9aezsu8qfflxr-dc20b9d73fdc6e87bab2b3935f8d2e2c/image_view.jpeg
-# https://s3.amazonaws.com/rentspaces/uploads/1393787711815-9ul6lz3r6gsnhfr-7f0797ab436b8098227f8d3abcf09c82/Original-Empty-Room-Resized.jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393787719281-qbjt34jv4bpm0a4i-7f0797ab436b8098227f8d3abcf09c82/I%2Bbelieve%2Bit%2Bwould%2Bbe%2Bsomething%2Blike%2Bthis%2B_93d4703edf64676729594cc837f99582.jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393790193377-49930nsk2urtfbt9-c423f8f12cc74fb17dc49585590f0ca5/177904033_80ebca4fd3_o.jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393791233711-tqnasgt4sjg7gb9-1e54f0261d5ad5fbd9d89db420efef14/dance_space.jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393793550915-avlx57x6sguy2e29-e4a45dc51eacfeb964193f208e908e2b/images.jpeg
-# https://s3.amazonaws.com/rentspaces/uploads/1393794127242-ugv41c3owv2yy14i-051bd9a4f9b4afe9b19fa26c4c4d15e8/1251910935-yd-a-528x343.jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393793556754-hfngcnc56gmygb9-e4a45dc51eacfeb964193f208e908e2b/Rosana-empty-studio.jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393794127242-ugv41c3owv2yy14i-051bd9a4f9b4afe9b19fa26c4c4d15e8/1251910935-yd-a-528x343.jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393857056084-zvalncahh6ywrk9-a57e6ea588502e0309282eaf9698e8a5/mms_picture+copy.jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393857071031-13nwxwzd1ln89f6r-a57e6ea588502e0309282eaf9698e8a5/mms_picture.jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393861299877-44fahvii1j68ncdi-73c797abf11bbd6d22cfba3f2e4a4468/mms_picture+(1)+copy.jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393861381975-bddxfzkxfyxh6w29-e876072773b4cce0fc818245fad93b75/mms_picture+(1).jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393861452673-r7pjok8clult0529-c4698ce628f9862e1d1e695d92055ea7/mms_picture+(2).jpg
-# https://s3.amazonaws.com/rentspaces/uploads/1393874804694-b8rgrf4rf3d9ggb9-62a79ae1c92acddfd4450b3883a66465/sanfran.jpg
+# "https://s3.amazonaws.com/rentspaces/uploads/1393785811893-n0yau8vrclo0qkt9-4c20bbe51412da2e07a6b3b216b998f7/download.jpeg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393786143147-0esjp61262zqia4i-54f4514197f477b174bab6c11a466241/dl2.jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393786148919-dxo28k26efef80k9-54f4514197f477b174bab6c11a466241/tumblr_l00r7wG6xt1qzoh1b.jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393786238790-gwip961oje3a0pb9-4ea79c2f649e7b192497a0571ab54669/south_space_large.jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393786528482-pj9aezsu8qfflxr-dc20b9d73fdc6e87bab2b3935f8d2e2c/image_view.jpeg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393787711815-9ul6lz3r6gsnhfr-7f0797ab436b8098227f8d3abcf09c82/Original-Empty-Room-Resized.jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393787719281-qbjt34jv4bpm0a4i-7f0797ab436b8098227f8d3abcf09c82/I%2Bbelieve%2Bit%2Bwould%2Bbe%2Bsomething%2Blike%2Bthis%2B_93d4703edf64676729594cc837f99582.jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393790193377-49930nsk2urtfbt9-c423f8f12cc74fb17dc49585590f0ca5/177904033_80ebca4fd3_o.jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393791233711-tqnasgt4sjg7gb9-1e54f0261d5ad5fbd9d89db420efef14/dance_space.jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393793550915-avlx57x6sguy2e29-e4a45dc51eacfeb964193f208e908e2b/images.jpeg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393794127242-ugv41c3owv2yy14i-051bd9a4f9b4afe9b19fa26c4c4d15e8/1251910935-yd-a-528x343.jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393793556754-hfngcnc56gmygb9-e4a45dc51eacfeb964193f208e908e2b/Rosana-empty-studio.jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393794127242-ugv41c3owv2yy14i-051bd9a4f9b4afe9b19fa26c4c4d15e8/1251910935-yd-a-528x343.jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393857056084-zvalncahh6ywrk9-a57e6ea588502e0309282eaf9698e8a5/mms_picture+copy.jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393857071031-13nwxwzd1ln89f6r-a57e6ea588502e0309282eaf9698e8a5/mms_picture.jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393861299877-44fahvii1j68ncdi-73c797abf11bbd6d22cfba3f2e4a4468/mms_picture+(1)+copy.jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393861381975-bddxfzkxfyxh6w29-e876072773b4cce0fc818245fad93b75/mms_picture+(1).jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393861452673-r7pjok8clult0529-c4698ce628f9862e1d1e695d92055ea7/mms_picture+(2).jpg",
+# "https://s3.amazonaws.com/rentspaces/uploads/1393874804694-b8rgrf4rf3d9ggb9-62a79ae1c92acddfd4450b3883a66465/sanfran.jpg"
 #25
 
 
