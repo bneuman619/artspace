f0863d3a734f18ff4d9a52693233e09443e1f430
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-04 20:14:22 -0600

    Cleaning up the JS

---
+++ app/assets/javascripts/calendar/make_availability_calendar.js
@@ -0,0 +1,60 @@
+function make_availability_calendar(events_input) {
+  var calendar = {
+    // scrollToHourMillis : 0,
+    // timeslotsPerHour: 2,
+    // timeslotHeight: 20,
+    // displayOddEven: true,
+    // allowEventDelete: true,
+    // daysToShow: 7,
+    // headerSeparator: ' ',
+    // allowCalEventOverlap: false,
+    // businessHours: false,
+
+    dateFormat: 'd F y',
+    defaultFreeBusy: {free: true},
+    displayFreeBusys: false,
+    useShortDayNames: false,
+
+    buttons: false,
+    
+
+    height: function($calendar){
+      return $(window).height() - $('h1').outerHeight(true);
+    },
+
+    data: function(start, end, callback) {
+      callback(events_input);
+    },
+
+    title: function() {
+      return "Mark the weekly business hours for your space on the calendar below";
+    },
+
+    eventNew: function (calEvent) {
+      calEvent.id = generateEventId();
+    },
+
+    
+
+    eventDelete: function(calEvent, element, dayFreeBusyManager, calendar, clickEvent) {
+      if (confirm('You want to delete this availability slot?')) {
+        calendar.weekCalendar('removeEvent',calEvent.id);
+      }
+    },
+   
+    getHeaderDate: function(date, calendar) {
+      return get_day(date);
+    },
+
+
+    eventBody: function() {
+      return "Available hours";
+    },
+
+   
+
+  }
+
+  return calendar;
+
+}
--- app/assets/javascripts/calendar/show_calendar.js
+++ app/assets/javascripts/calendar/show_calendar.js
@@ -7,23 +7,11 @@ function show_renter_calendar(selector, read_only){
 }
 
 function show_creation_calendar(selector){
-  var eventData = {
-    events: [],
-    freebusys: [],
-  };
-
-  var calendar = make_creation_calendar(eventData);
+  var calendar = make_availability_calendar(parse_for_new_availabilities());
   $(selector).weekCalendar(calendar);
 }
 
 function show_edit_availability_calendar(selector) {
-  var data = get_calendar_data();
-  console.log(data);
-  var parsed = parse_for_availability_edit(data);
-
-  var calendar = make_availability_edit_calendar(parse_for_availability_edit(get_calendar_data()));
+  var calendar = make_availability_calendar(parse_for_availability_edit(get_calendar_data()));
   $(selector).weekCalendar(calendar);
 }
-
-
-
