3dacab92f056677b4162546b2f74949800969d5b
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-04 20:42:49 -0600

    Fixed login logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_or_create_by(email: params[:email])
+    user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:current_user_id] = user.id
       redirect_to user_path(user)
     else
       flash.now.alert = "Invalid email or password"
-      render root_url
+      redirect_to root_url
     end
   end
 
